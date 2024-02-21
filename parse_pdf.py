1e1affceda4ea3bf18c840a0b506c90d411cb723
terryobe-ncu <113522238+terryobe-ncu@users.noreply.github.com>
2024-02-21 16:09:54 +0800

    Add files via upload

--- parse_pdf.py
+++ parse_pdf.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import os
+from io import StringIO
+from re import compile
+
+# !pip install pdfminer.six
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+DEMO_COLOR = '\33[93m'  # yellow
 
 
 def stem_name(path: str) -> str:
@@ -8,16 +20,112 @@ def stem_name(path: str) -> str:
     return base_name[: base_name.rfind('.')]
 
 
-def pdf2csv(pdf_path: str, output_dir='') -> NotImplemented:
-    df = pd.DataFrame(
-        {
-            'paragraph':
-                (
-                    'pdf2csv has not been implemented yet, this is a sample csv',
-                    'pdf2csv 尚未被實作，這是範例csv',
-                    'today our teams around the world infuse apples deeply held values into everything we make that work can take many forms but whether we are protecting the right to privacy designing technology that is accessible to all or using more recycled material in our products than ever we are always working to make a difference for the people we serve and the planet we inhabit'
-                )
-        }
-    )
+def pdf2str(pdf_path: str) -> str:
+    output_string = StringIO()
+    with open(pdf_path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+    return output_string.getvalue()
+
+
+_REG_FIND_REPEAT = compile(r'(\D+)\1{4,}')  # consecutively repeated (at least 5 times) adjacent substrings
+
+
+class DocumentExtractor:
+    def __init__(self, document: str, is_chinese=None):
+        self.IS_CHINESE = self.is_chinese_document_(document) if is_chinese is None else is_chinese
+        self.PERIOD = '。' if self.IS_CHINESE else '.'
+        self.LINES = document.split('\n')
+        self._JOIN = '' if self.IS_CHINESE else ' '
+        self._demo = []
+
+    def extract(self) -> list[str]:
+        self._demo.clear()
+        result, stack = [], []
+        for line in self.LINES:
+            if not line.strip():  # ignore empty line
+                if stack:
+                    stack[-1] += '\n'
+                else:
+                    self._demo.append(line)
+                continue
+            if 'http' in line or _REG_FIND_REPEAT.search(line):  # bad content
+                stack.append(line)
+                self._demo.extend(stack)
+                stack.clear()
+                continue
+
+            meaningful = self.is_meaningful(line, self.IS_CHINESE)
+            have_period = line[-10:].count(self.PERIOD) == 1
+
+            if not stack:
+                if meaningful:
+                    if have_period:
+                        result.append(line)
+                        self._demo.append(DEMO_COLOR + line + '\33[0m')
+                    else:
+                        stack.append(line)
+                else:
+                    self._demo.append(line)
+                continue
+
+            # stack_have_period = stack[-1][-10:].count(self.PERIOD) == 1
+            if have_period:
+                if len(line) > len(stack[-1]) * 1.3:
+                    self._demo.extend(stack)
+                    stack.clear()
+                stack.append(line)
+                result.append(self._JOIN.join(stack))
+                self._demo.append(DEMO_COLOR + '\n'.join(stack) + '\33[0m')
+                stack.clear()
+            elif self.are_similar_lengths(stack[-1], line):
+                stack.append(line)
+            else:
+                self._demo.extend(stack)
+                stack = [line] if meaningful else []
+
+        self._demo.extend(stack)
+        # rstrip period and remove '\n'
+        result = [s[:s.rfind(self.PERIOD)].replace('\n', '') for s in result]
+        return result
+
+    def demo(self):
+        if not self._demo:
+            self.extract()
+        print('\n'.join(self._demo))
+
+    @staticmethod
+    def are_similar_lengths(a: str, b: str) -> bool:
+        return 0.7 < len(a) / len(b) < 1.3
+
+    @staticmethod
+    def is_chinese_document_(document: str) -> bool:
+        c = 0
+        bound = min(100, len(document) >> 1)
+        for u in map(ord, document[:500]):
+            if u >= 19968:
+                c += 1
+                if c > bound:
+                    return True
+        return False
+
+    @staticmethod
+    def is_meaningful(s: str, is_chinese: bool) -> bool:
+        if is_chinese:
+            return sum(u >= 19968 for u in map(ord, s)) >= 10
+        return sum(len(c) > 1 for c in s.split()) >= 5 or ('.' in s[:-5] and len(s) >= 20)
+
+
+def pdf2csv(pdf_path: str, output_dir='') -> DocumentExtractor:
+    document = pdf2str(pdf_path)
+    extractor = DocumentExtractor(document)
+    paragraph = extractor.extract()
+    df = pd.DataFrame({'paragraph': paragraph})
     csv_path = os.path.join(output_dir, stem_name(pdf_path) + '.csv')
     df.to_csv(csv_path, sep=',', index=False, encoding='utf-8')
+    return extractor
